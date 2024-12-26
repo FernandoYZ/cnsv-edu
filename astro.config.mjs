00c669b23582a90b8873fa2a44340b04d86ea06f
FernandoYZ <fernandozavala266@gmail.com>
2024-12-25 19:49:52 -0500

    chore(config): actualizar configuraciones en astro.config.mjs
    Se añadieron múltiples configuraciones para optimizar el rendimiento y la construcción del proyecto:
    - `output: 'static'` para especificar la salida como estática.
    - `compressHTML: true` para habilitar la compresión de HTML.
    - Ajustes en `build`, incluyendo `inlineStylesheets: 'auto'` y opciones para minificación CSS y uso de Terser.
    - Configuración de `rollupOptions` con `manualChunks` para dividir paquetes en `vendor` y `ui`.
    - Implementación de un plugin `gzip` para la compresión post-construcción.
    
    #Closes 0

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,43 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    integrations: [tailwind()],
+    output: 'static',
+    compressHTML: true,
+    build: {
+        inlineStylesheets: 'auto'
+    },
+    vite: {
+        build: {
+            cssMinify: true,
+            minify: 'terser',
+            cssCodeSplit: false,
+            terserOptions: {
+                compress: {
+                    drop_console: true,
+                    passes: 2
+                },
+                mangle: true
+            },
+            rollupOptions: {
+                output: {
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return 'vendor';
+                        }
+                        if (id.includes('ui-library')) {
+                            return 'ui';
+                        }
+                    }
+                }
+            }
+        },
+        plugins: [
+            {
+                name: 'gzip',
+                apply: 'build',
+                enforce: 'post'
+            }
+        ]
+    }
 });
