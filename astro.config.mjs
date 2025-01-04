9cfea31bdbc32103f54de68f446e6a067245bfac
FernandoYZ <fernandozavala266@gmail.com>
2025-01-04 12:14:53 -0500

    feat(vercel): añadir vercel.json optimizando el SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://cnsv-edu.vercel.app/',
     integrations: [tailwind()],
     output: 'static',
     compressHTML: true,
