b5a379a08c5eaf440f57993fd243eeb4e68ab6a8
Fernando Yovera Zavala <141245158+FernandoYZ@users.noreply.github.com>
2025-02-19 12:39:19 -0500

    fix(config): actualizar configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,54 +1,32 @@
 // @ts-check
+
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://cnsv-edu.vercel.app/',
-    integrations: [tailwind(), sitemap({
-        changefreq: 'weekly',
-        priority: 0.8
-    })],
-    output: 'static',
-    compressHTML: true,
+  site: 'https://cnsv-edu.vercel.app/',
+  output: 'server',
+  adapter: vercel(),
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.8
+    })
+  ],
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto'
+  },
+  vite: {
     build: {
-        inlineStylesheets: 'auto'
-    },
-    vite: {
-        build: {
-            cssMinify: true,
-            minify: 'terser',
-            cssCodeSplit: true,
-            terserOptions: {
-                compress: {
-                    drop_console: false,
-                    passes: 1
-                },
-                mangle: false
-            },
-            rollupOptions: {
-                output: {
-                    manualChunks(id) {
-                        if (id.includes('node_modules')) return 'vendor';
-                    }
-                }
-            }
-        },
-        plugins: [
-            {
-                name: 'gzip',
-                apply: 'build',
-                enforce: 'post'
-            }
-        ],
-        server: {
-            hmr: {
-                overlay: false
-            }
-        }
-    },
+      cssMinify: true,
+      cssCodeSplit: false,
+    }
+  }
 });
